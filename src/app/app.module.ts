4bcb3ceae2b5d0c6ea7042f0066033d11d00729c
Joel <xsrjowi@outlook.es>
2023-07-07 17:06:43 +0200

    project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './dashboard/pages/mainpage/mainpage.component';
+import { CalendarComponent } from './dashboard/components/calendar/calendar.component';
+import { EventCardComponent } from './dashboard/components/event-card/event-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    CalendarComponent,
+    EventCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@NgModule({
+    declarations: [
+        LayoutComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+            CommonModule,
+            MaterialModule,
+            AuthModule,
+    ]
+})
+
+export class AuthModule { }
